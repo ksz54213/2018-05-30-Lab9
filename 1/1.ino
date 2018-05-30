b08610a7162e14107700c830dcb4e12300b5f716
ksz54213 <36830376+ksz54213@users.noreply.github.com>
2018-05-30 15:16:21 +0800

    Add files via upload

---
+++ 1/1.ino
@@ -0,0 +1,22 @@
+
+#include <IRremote.h>
+const int irReceiverPin = 12;
+IRrecv irrecv(irReceiverPin);
+decode_results results;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(irrecv.decode(&results)){
+    Serial.print(" irCode: ");
+    Serial.print(results.value,HEX);
+    Serial.print(",  bits: ");
+    Serial.print(results.bits);
+    irrecv.resume();
+  }
+}
---
+++ 2/2.ino
@@ -0,0 +1,19 @@
+#include<IRremote.h>
+
+int buttonSate =0;
+IRsend irsend ;
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonSate = digitalRead(6);
+  if(buttonSate){
+    digitalWrite(3,1);
+    irsend.sendNEC(0x4FB48B7,32);
+  }
+  delay(100);
+  digitalWrite(3,0);
+}
